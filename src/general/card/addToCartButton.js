d7f7053170892f51fe0bf7d22a2880aa7491c17b
noushkia <kianoosharshi@gmail.com>
2023-04-26 08:58:42 +0330

    Separate AddToCart button component for reusability

---
+++ src/general/card/addToCartButton.js
@@ -0,0 +1,60 @@
+import {updateInCart} from "../../utils/api/Users";
+import {toast} from "react-toastify";
+import React, {Fragment} from "react";
+import "./card.css";
+
+function addToCartButton(props) {
+    const handleCartUpdate = (commodityId, quantity) => {
+        updateInCart(commodityId, quantity)
+            .then(commodity => {
+                console.log(`RemoveFromCart: success! ${commodityId}`);
+            }).catch(error => {
+            if (error.response) {
+                console.log(error.response.data);
+                toast.error(error.response.data.error);
+            } else {
+                console.log('RemoveFromCart: server down?');
+                toast.error('Server not responding');
+            }
+        });
+    };
+
+    return (
+        <Fragment>
+            {
+                props.inCart ?
+                    <div className="btn-group">
+                        <button
+                            className="btn"
+                            type="button"
+                            onClick={() => handleCartUpdate(props.id, -1)}
+                        > todo: check if the button is updated when item is removed using -
+                            -
+                        </button>
+                        <button className="btn" type="button" disabled>
+                            {props.inCart}
+                        </button>
+                        <button
+                            className="btn"
+                            type="button"
+                            onClick={() => handleCartUpdate(props.id, 1)}
+                            disabled={props.inCart === props.inStock}
+                        >
+                            +
+                        </button>
+                    </div>
+                    :
+                    <button
+                        className="btn"
+                        type="button"
+                        onClick={() => handleCartUpdate(props.id, 1)}
+                        disabled={props.inStock === 0}
+                    >
+                        {props.inStock === 0 ? 'Sold Out' : 'Add to Cart'}
+                    </button>
+            }
+        </Fragment>
+    )
+}
+
+export default addToCartButton;
